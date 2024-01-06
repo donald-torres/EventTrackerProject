e54532a69542db86ea1f727e5dcac3f18971e19b
Donald Torres <dctorres5597@gmail.com>
2024-01-05 19:25:34 -0500

    Configure spring REST project, connect JPA project dependencies

---
+++ BingeWorthy/src/main/java/com/skilldistillery/bingeworthy/BingeWorthyApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bingeworthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingeWorthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingeWorthyApplication.class, args);
+	}
+
+}
---
+++ BingeWorthy/src/test/java/com/skilldistillery/bingeworthy/BingeWorthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bingeworthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingeWorthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
